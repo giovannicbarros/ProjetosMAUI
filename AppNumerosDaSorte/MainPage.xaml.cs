2d1fae6ff053e3c6bda796d19bc9833ffccaac6b
Giovanni <gcgbarros@gmail.com>
2025-04-16 14:11:21 -0300

    App tela e código funcionando

--- AppNumerosDaSorte/MainPage.xaml.cs
+++ AppNumerosDaSorte/MainPage.xaml.cs
@@ -6,4 +6,33 @@ public partial class MainPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+	private void OnGenerateLuckNumbers(object sender, EventArgs e)
+	{
+		NameApp.IsVisible = false;
+		ContainerLuckNumbers.IsVisible = true;
+
+		var set = GenerateNumbers();
+		NumberLuck01.Text = set.ElementAt(0).ToString("D2");
+        NumberLuck02.Text = set.ElementAt(1).ToString("D2");
+        NumberLuck03.Text = set.ElementAt(2).ToString("D2");
+        NumberLuck04.Text = set.ElementAt(3).ToString("D2");
+        NumberLuck05.Text = set.ElementAt(4).ToString("D2");
+        NumberLuck06.Text = set.ElementAt(5).ToString("D2");
+    }
+
+    private SortedSet<int> GenerateNumbers()
+	{
+		var set = new SortedSet<int>();
+		while (set.Count < 6)
+		{
+            var random = new Random();
+            var luckNumber = random.Next(1, 60);
+
+			set.Add(luckNumber);
+        }
+
+		return set;
+    }
+
 }
