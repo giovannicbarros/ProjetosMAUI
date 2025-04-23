53d5667204c7506c4acb38396d999aad2cc21a03
Giovanni <gcgbarros@gmail.com>
2025-04-23 08:57:28 -0300

    component StackLayout

---
+++ AppMAUIGalery/Repositories/CategoryRepository.cs
@@ -0,0 +1,31 @@
+﻿using AppMAUIGallery.Models;
+using AppMAUIGallery.Views.Layouts;
+
+namespace AppMAUIGallery.Repositories
+{
+    internal class CategoryRepository
+    {
+        public CategoryRepository()
+        {
+                
+        }
+
+        public List<Category> GetCategories()
+        {
+            List<Category> categories = new List<Category>();
+            categories.Add(new Category
+            {
+                Name = "Layout",
+                Components = new List<Component> { 
+                    new Component {
+                        Title = "StackLayout",
+                        Description = "Organização squencia dos elementos.",
+                        Page = typeof(StackLayoutPage)
+                    }
+                }
+            });
+
+            return categories;
+        }
+    }
+}
