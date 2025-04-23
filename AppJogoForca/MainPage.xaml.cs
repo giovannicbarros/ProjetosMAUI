52b5c19af66af9c8ee94fd181d06f8032e9300f0
Giovanni <gcgbarros@gmail.com>
2025-04-23 15:41:39 -0300

    Programação inicial - Escolher a palavra

--- AppJogoForca/MainPage.xaml.cs
+++ AppJogoForca/MainPage.xaml.cs
@@ -1,11 +1,27 @@
-﻿namespace AppJogoForca
+﻿using AppJogoForca.Models;
+using AppJogoForca.Repositories;
+
+namespace AppJogoForca
 {
     public partial class MainPage : ContentPage
     {
 
+        private Word _word;
+
         public MainPage()
         {
             InitializeComponent();
+
+            var repository = new WordRepositories();
+            _word = repository.GetRandleWord();
+
+            lblTips.Text = _word.Tips;
+            lblText.Text = new string('_', _word.Text.Length);
+        }
+
+        private void OnButtonClicked(object sender, EventArgs e)
+        {
+
         }
        
     }
---
+++ AppJogoForca/Repositories/WordRepositories.cs
@@ -0,0 +1,33 @@
+﻿using AppJogoForca.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppJogoForca.Repositories
+{
+    public class WordRepositories
+    {
+        private List<Word> _words;
+
+        public WordRepositories()
+        {
+                _words = new List<Word>();
+                _words.Add(new Word("Nome", "Maria"));
+                _words.Add(new Word("Vegetal", "Cenoura"));
+                _words.Add(new Word("Fruta", "Abacate"));
+                _words.Add(new Word("Carro", "Renegade"));
+                _words.Add(new Word("Moto", "Ducati"));
+                _words.Add(new Word("Moto", "Yamaha"));
+        }
+
+        public Word GetRandleWord()
+        {
+            Random random = new Random();
+            var number = random.Next(0, _words.Count);
+
+            return _words[number];
+        }
+    }
+}
