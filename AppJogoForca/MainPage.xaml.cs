a275e61b801144d05b421af5815a2a016f41634e
Giovanni <gcgbarros@gmail.com>
2025-04-23 16:37:45 -0300

    finalização do jogo

--- AppJogoForca/MainPage.xaml.cs
+++ AppJogoForca/MainPage.xaml.cs
@@ -8,20 +8,15 @@ namespace AppJogoForca
     {
 
         private Word _word;
-        private int _errors = 0;
+        private int _errors;
 
         public MainPage()
         {
             InitializeComponent();
-
-            var repository = new WordRepositories();
-            _word = repository.GetRandleWord();
-
-            lblTips.Text = _word.Tips;
-            lblText.Text = new string('_', _word.Text.Length);
+            ResetScreen();
         }
 
-        private void OnButtonClicked(object sender, EventArgs e)
+        private async void OnButtonClicked(object sender, EventArgs e)
         {
             ((Button)sender).IsEnabled = false;
             string letter = ((Button)sender).Text;
@@ -31,6 +26,14 @@ namespace AppJogoForca
             if (positions.Count == 0)
             {
                 _errors++;
+                ImgForca.Source = ImageSource.FromFile($"forca{_errors + 1}.png");
+
+                if (_errors == 6)
+                {
+                    await DisplayAlert("Perdeu!", "Você foi enforcado, quer começar um novo jogo?", "Novo Jogo");
+                    ResetScreen();
+                }
+
                 return;
             }
 
@@ -38,7 +41,42 @@ namespace AppJogoForca
                 lblText.Text = lblText.Text.Remove(position, 1).Insert(position, letter);
             }
         }
-       
+
+        private void ResetScreen()
+        {
+            ResetVirtualKeyBoard();
+            ResetErrors();
+            GenerateNewWord();
+        }
+
+        private void GenerateNewWord()
+        {
+            var repository = new WordRepositories();
+            _word = repository.GetRandleWord();
+
+            lblTips.Text = _word.Tips;
+            lblText.Text = new string('_', _word.Text.Length);
+        }
+
+        private void ResetErrors()
+        {
+            _errors = 0;
+            ImgForca.Source = ImageSource.FromFile("forca1.png");
+        }
+
+        private void ResetVirtualKeyBoard()
+        {
+            ResetVirtualLines((HorizontalStackLayout)KeyboardContainer.Children[0]);
+            ResetVirtualLines((HorizontalStackLayout)KeyboardContainer.Children[1]);
+            ResetVirtualLines((HorizontalStackLayout)KeyboardContainer.Children[2]);
+        }
+
+        private void ResetVirtualLines(HorizontalStackLayout horizontal)
+        {
+            foreach (Button button in horizontal.Children) { 
+                button.IsEnabled = true;
+            }
+        }
     }
 
 }
